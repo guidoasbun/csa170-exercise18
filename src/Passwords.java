e8a68db76559e6fb4a0f7bc0e991c0bffb2da247
guidoasbun <gdrodrigo8@gmail.com>
2020-10-21 15:53:57 -0700

    Part B init

---
+++ src/Passwords.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Passwords {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[] usernames = {"Matt","Mark","Luke","John","Paul"};
+        String[] passwords = {"king","slave","human","divine","church"};
+        String name, pass;
+
+        System.out.print("Enter username: ");
+        name = in.next();
+        System.out.print("Enter password: ");
+        pass = in.next();
+
+
+    }
+}
