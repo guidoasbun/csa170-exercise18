a24ce51a1b6119556547de30c49c9da95bfc306e
guidoasbun <gdrodrigo8@gmail.com>
2020-10-21 16:44:51 -0700

    Part B added comments

--- src/Passwords.java
+++ src/Passwords.java
@@ -4,18 +4,24 @@ public class Passwords {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        //inits both arrays
+        //inits tries allowed to check
         String[] usernames = {"Matt","Mark","Luke","John","Paul"};
         String[] passwords = {"king","slave","human","divine","church"};
         String name, pass;
         int tries = 0;
         boolean access = false;
 
+        //used do loop because I want the operations to run at least once
         do{
+            //reads user input and assigns to String variables
             System.out.print("Enter username: ");
             name = in.next();
             System.out.print("Enter password: ");
             pass = in.next();
 
+            //checks trough the usernames array 1st, then checks passwords array if match
+            //assigns true to access if both are equals
             for(int i = 0; i < usernames.length; i++){
                 if(name.equals(usernames[i])){
                     if(pass.equals(passwords[i])){
@@ -24,6 +30,7 @@ public class Passwords {
                 }
             }
 
+            //prints depending on what the status if access is
             if (access){
                 System.out.println("Access Granted!");
             }else{
