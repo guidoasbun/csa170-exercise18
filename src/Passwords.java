0e8e2f9de0175c6ac2a4d2aa043e2b1fd8b04594
guidoasbun <gdrodrigo8@gmail.com>
2020-10-21 16:31:14 -0700

    Part B complete

--- src/Passwords.java
+++ src/Passwords.java
@@ -7,12 +7,31 @@ public class Passwords {
         String[] usernames = {"Matt","Mark","Luke","John","Paul"};
         String[] passwords = {"king","slave","human","divine","church"};
         String name, pass;
+        int tries = 0;
+        boolean access = false;
 
-        System.out.print("Enter username: ");
-        name = in.next();
-        System.out.print("Enter password: ");
-        pass = in.next();
+        do{
+            System.out.print("Enter username: ");
+            name = in.next();
+            System.out.print("Enter password: ");
+            pass = in.next();
 
+            for(int i = 0; i < usernames.length; i++){
+                if(name.equals(usernames[i])){
+                    if(pass.equals(passwords[i])){
+                        access = true;
+                    }
+                }
+            }
 
+            if (access){
+                System.out.println("Access Granted!");
+            }else{
+                System.out.println("Sorry, your username/password does not match our" +
+                        "database. Contact the administrator");
+            }
+
+            tries++;
+        }while(tries < 2 && !access);
     }
 }
