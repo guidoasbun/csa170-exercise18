dee4f723f1e620135211bcdd2dbb06445b15d698
guidoasbun <gdrodrigo8@gmail.com>
2020-10-21 11:16:56 -0700

    initial

---
+++ src/ArraysAndLoops.java
@@ -0,0 +1,10 @@
+public class ArraysAndLoops {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for(int i = 0; i < arr1.length; i++){
+
+        }
+
+    }
+}
